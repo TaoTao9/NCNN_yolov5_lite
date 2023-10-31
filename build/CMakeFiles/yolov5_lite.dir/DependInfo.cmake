e23c4c4c33c8512bc4d79a49ffb9d4d9b82fb7e2
taotao9 <2406003127@qq.com>
2023-10-31 12:19:47 +0800

    summit project file!

---
+++ build/CMakeFiles/yolov5_lite.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/tao_workspace/test_yolov5lite/yolov5_lite_e.cpp" "/home/pi/tao_workspace/test_yolov5lite/build/CMakeFiles/yolov5_lite.dir/yolov5_lite_e.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/pi/Downloads/inference_engine_vpu_arm/opencv/include"
+  "../ncnn/include/ncnn"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
